cf5600bfbf6a0047f786c580e4e42cd8aa2eaa04
adityak-bit <adityajsr1998@gmail.com>
2020-10-07 12:47:48 +0530

    [ADITYA] Refactored UC5

--- com/example/TicTacToeGame.java
+++ com/example/TicTacToeGame.java
@@ -6,33 +6,38 @@ public class TicTacToeGame {
 	char[] board = new char[10];
 	private static char move;
 	private Scanner sc;
-	private int index;
-	
+	private static int index ;
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe game");
 		TicTacToeGame object = new TicTacToeGame();
-		System.out.println("Choose your move index");
-		object.userMove();
-		object.showBoard(move);
-	}
-	
-	public void showBoard(char c) {
+		while(index < 10) {
 		
+		System.out.println("Choose your move index from 1 - 9");
+			object.userMove();
+			object.showBoard(move);
+			}
+		
+	}
+
+	public void showBoard(char c) {
+
 		for (int i = 1; i < board.length; i = i + 3) {
-			board[index] =c;
+			board[index] = c;
 			System.out.println("------------------");
-			System.out.println(board[i] + "\t" + board[i + 1] + "\t" + board[i + 2]+ "\n");
+			System.out.println(board[i] + "\t" + board[i + 1] + "\t" + board[i + 2] + "\n");
 		}
 	}
-	
+
 	public void userMove() {
 		sc = new Scanner(System.in);
 		int index = sc.nextInt();
-		if(board[index] != 'X' && board[index] != 'O') {
-			board[index] = 'O';
-		    move = board[index];
-		}
-		else
+		if (board[index] != 'X' && board[index] != 'O') {
+			System.out.println("Enter your move X or O");
+			char ch = sc.next().charAt(0);
+			board[index] = ch;
+			move = board[index];
+		} else
 			System.out.println("Index not free");
 	}
 }
