62f2b99c1a2d594c306cc8426dc97c03fd8a561f
adityak-bit <adityajsr1998@gmail.com>
2020-10-08 08:41:23 +0530

    [ADITYA] Added : isWinner() to UC7

--- com/example/TicTacToeGame.java
+++ com/example/TicTacToeGame.java
@@ -1,20 +1,23 @@
 package com.example;
 
 public class TicTacToeGame {
-	public static final int HEAD = 0;
-	public static final int TAIL = 1;
-
-	public static enum Player {
-		USER, COMPUTER
-	};
-
+	
 	public static void main(String[] args) {
-		Player player = getWhoStartsFirst();
+		System.out.println("Check if won: " +isWinner(board,userLetter));
 	}
+	
+	
+	private static boolean isWinner(char[] b, char ch) {
+		return((b[1] == ch && b[2] == ch && b[3] == ch) || //across the top
+			(b[4] == ch && b[5] == ch && b[6] == ch) || //across the middle
+			(b[7] == ch && b[8] == ch && b[9] == ch) || //across the bottom
+			(b[1] == ch && b[4] == ch && b[7] == ch) || //across the left
+			(b[2] == ch && b[5] == ch && b[8] == ch) || //across the middle
+			(b[3] == ch && b[6] == ch && b[9] == ch) || //across the right
+			(b[1] == ch && b[5] == ch && b[9] == ch) || //across the leading diagonal
+			(b[3] == ch && b[5] == ch && b[7] == ch) );	//across the other diagonal
+		}
+	}
+		
+	
 
-	/* UC6 */
-	private static Player getWhoStartsFirst() {
-		int toss = (int) (Math.random() * 10) % 2;
-		return (toss == HEAD) ? Player.USER : Player.COMPUTER;
-	}
-}
